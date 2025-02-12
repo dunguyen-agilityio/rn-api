433add4763850849b4958c92560466efc529946a
du.nguyen <du.nguyen@asnet.com.vn>
2025-02-12 08:46:26 +0700

    update server

---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("data/db.json"); // Change this to your data file
+const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 8080;
+
+server.use(middlewares);
+server.use(router);
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT}`);
+});

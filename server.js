0e1d23468f02d7427842b151e8dd528c3216f167
du.nguyen <du.nguyen@asnet.com.vn>
2025-02-18 09:25:19 +0700

    a

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 8080;
 
 server.use(middlewares);
+
 server.use(router);
 server.listen(PORT, () => {
     console.log(`JSON Server is running on port ${PORT}`);
